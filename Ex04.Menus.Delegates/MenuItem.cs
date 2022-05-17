8669dd63a5e0876ebe511b3705d90872798985bf
Snirpini <snirpinievsky@gmail.com>
2022-05-17 16:51:23 +0300

    started to write components

---
+++ Ex04.Menus.Delegates/MenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex04.Menus.Delegates
+{
+    public abstract class MenuItem
+    {
+        private readonly string r_Title;
+        public event Action<string> m_Report
+
+        public MenuItem(string i_Title)
+        {
+            r_Title = i_Title;
+        }
+
+        
+    }
+}
--- Ex04.Menus.Delegates/MenuTool.cs
+++ Ex04.Menus.Delegates/MenuTool.cs
@@ -3,13 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Ex04.Menus.Test
+namespace Ex04.Menus.Delegates
 {
-    class Program
+    class MenuTool : MenuItem
     {
-        public static void Main()
-        {
-
-        }
     }
 }
